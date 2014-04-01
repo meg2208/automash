bdd25fe271928d2d1b80c3b0cbd0cb05f3bce5dd
Matt Garbis <mgarbis@gmail.com>
2014-04-01 00:37:10 -0400

    don't print that

--- make_mashup.py
+++ make_mashup.py
@@ -3,7 +3,6 @@ from common import VALID_MOODS
 from sys import argv
 
 if __name__ == '__main__':
-	print 'hi'
 	if len(argv) != 3:
 		raise Exception("Please try again using this format:\npython make_mashup.py <mood> <output_name>")
 	elif argv[1] not in VALID_MOODS:
